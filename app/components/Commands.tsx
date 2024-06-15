fca8ac877c716024ee77cb0986f657e969db808e
Supreme Khadka <supremekhadka2@gmail.com>
2024-06-15 13:50:14 +0545

    favicon update and minor fixes

--- app/components/Commands.tsx
+++ app/components/Commands.tsx
@@ -1,8 +1,8 @@
 export const commands = {
-    help: `Available Commands: hello, email, phone, and clear`,
+    help: `Available Commands: \n\nhello: About Me \nemail: My Mail Id \ncontact: Phone Number \nportfolio: Portfolio \ncv: Download my CV \nclear: Clear Terminal`,
     hello: 'Hi, I am Supreme Khadka, an aspiring CS student, web developer and graphics designer.',
     email: 'Mail me at: supremekhadka2@gmail.com',
-    contact: 'Call me at: 9861584426',
+    contact: 'Call me at: +977 986 158 4426',
     portfolio: 'I will be uploading it soon ...',
     cv: 'I have sent you my CV, please check your Downloads!',
     notFound: `Oops! Looks like we don't have that command yet. Command not found: `
