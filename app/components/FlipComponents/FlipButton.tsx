78de8a1a45bafec4261b8fbf5226544370e15257
Supreme Khadka <supremekhadka2@gmail.com>
2024-09-05 23:56:11 +0545

    create flip button

---
+++ app/components/FlipComponents/FlipButton.tsx
@@ -0,0 +1,78 @@
+import React from "react";
+import FlipText from "./FlipText";
+
+import { motion } from "framer-motion";
+
+const DURATION = 0.25;
+const STAGGER = 0.025;
+
+const FlipButton = ({
+  children,
+  href = "",
+  className = "",
+}: {
+  children: string;
+  href?: string;
+  className?: string;
+}) => {
+  return (
+    <motion.a
+      href={href}
+      initial="initial"
+      whileHover="hovered"
+      className={`relative block overflow-hidden whitespace-nowrap ${className}`}
+      style={{
+        lineHeight: 1.075,
+      }}
+    >
+      <div>
+        {children.split("").map((l, i) => (
+          <motion.span
+            variants={{
+              initial: {
+                y: 0,
+              },
+              hovered: {
+                y: "-102%",
+              },
+            }}
+            transition={{
+              duration: DURATION,
+              ease: "easeInOut",
+              delay: STAGGER * i,
+            }}
+            className="inline-block"
+            key={i}
+          >
+            {l}
+          </motion.span>
+        ))}
+      </div>
+      <div className="absolute inset-0">
+        {children.split("").map((l, i) => (
+          <motion.span
+            variants={{
+              initial: {
+                y: "100%",
+              },
+              hovered: {
+                y: 0,
+              },
+            }}
+            transition={{
+              duration: DURATION,
+              ease: "easeInOut",
+              delay: STAGGER * i,
+            }}
+            className="inline-block"
+            key={i}
+          >
+            {l}
+          </motion.span>
+        ))}
+      </div>
+    </motion.a>
+  );
+};
+
+export default FlipButton;
--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React from "react";
 import FlipText from "./FlipComponents/FlipText";
+import FlipButton from "./FlipComponents/FlipButton";
 
 const Header = () => {
   return (
@@ -11,8 +12,8 @@ const Header = () => {
         <div className="h-8 overflow-hidden flex flex-col  items-center justify-start">
           <FlipText className="font-semibold">portfolio</FlipText>
         </div>
-        <button className="text-amber-500 font-bold cursor-pointer">
-          menu
+        <button className="text-amber-500  cursor-pointer">
+          <FlipButton className="font-bold">menu</FlipButton>
         </button>
       </div>
     </>
